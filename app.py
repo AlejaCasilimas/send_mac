e6163fc71569f0fef902a1b0e35ca3af4c378f34
AlejaCasilimas <143103499+AlejaCasilimas@users.noreply.github.com>
2023-11-02 10:08:37 -0500

    app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def on_message(client, userdata, message):
 
 broker="157.230.214.127"
 port=1883
-client1= paho.Client("MAlejaC")
+client1= paho.Client("MMM")
 client1.on_message = on_message
 
 
@@ -29,11 +29,11 @@ st.title("MQTT Control")
 
 if st.button('ON'):
     act1="ON"
-    client1= paho.Client("MAlejaC")                           
+    client1= paho.Client("MMM")                           
     client1.on_publish = on_publish                          
     client1.connect(broker,port)  
     message =json.dumps({"Act1":act1})
-    ret= client1.publish("mac_luces", message)
+    ret= client1.publish("MMMmqtt_s", message)
  
     #client1.subscribe("Sensores")
     
@@ -43,11 +43,11 @@ else:
 
 if st.button('OFF'):
     act1="OFF"
-    client1= paho.Client("MAlejaC")                           
+    client1= paho.Client("MMM")                           
     client1.on_publish = on_publish                          
     client1.connect(broker,port)  
     message =json.dumps({"Act1":act1})
-    ret= client1.publish("mac_luces", message)
+    ret= client1.publish("MMMmqtt_s", message)
   
     
 else:
@@ -57,11 +57,11 @@ values = st.slider('Selecciona el rango de valores',0.0, 100.0)
 st.write('Values:', values)
 
 if st.button('Enviar valor analógico'):
-    client1= paho.Client("MAlejaC")                           
+    client1= paho.Client("MMM")                           
     client1.on_publish = on_publish                          
     client1.connect(broker,port)   
     message =json.dumps({"Analog": float(values)})
-    ret= client1.publish("mac_motor", message)
+    ret= client1.publish("MMMmqtt_a", message)
     
  
 else:
